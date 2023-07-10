4b818fac9fa0ec9d3c82b641c23d77d528041b61
oluwafemimicheal <oluwafemicu@gmail.com>
2023-07-10 23:22:01 +0100

    initial commit

--- projectFile/easyFood.jsx
+++ projectFile/easyFood.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import {Header} from '../src/component/style'
+import {Header, Div, DivColor, Input} from '../src/component/style'
+import {BsSearch} from 'react-icons/bs'
 
 
 const EasyFood = () => {
     return (
         <>
             <Header>
-                
+                <Div>
+                    <div className="logo">
+                        <h1>easyFood</h1>
+                    </div>
+                    <span>
+                        <p>Delivery to</p>
+                        <small>Nigeria</small>
+                    </span>
+                    <div className="search-items flex jc gap-2">
+                        <select name="" id="">
+                            <option value="Food-bulk">All</option>
+                            <option value="Food-bulk">Food Bulk</option>
+                            <option value="Food-bulk">Food Middle</option>
+                            <option value="Food-bulk">Food Self</option>
+                            <option value="Food-bulk">Food All item</option>
+                            <option value="Food-bulk">Food Support Self</option>
+                            <option value="Food-bulk">Food Kitchen Light</option>
+                        </select>
+                        <Input type="text" />
+                        <BsSearch/>
+                    </div>
+                </Div>
+                <DivColor>
+                    <ul className='flex gap-3 ptd'>
+                        <li><a className='ac' href="#">All</a></li>
+                        <li><a className='ac' href="#">Today;s Deals</a></li>
+                        <li><a className='ac' href="#">Customer Service</a></li>
+                        <li><a className='ac' href="#">Registry</a></li>
+                        <li><a className='ac' href="#">Sell</a></li>
+                    </ul>
+                </DivColor>
             </Header>
         </>
     );
--- src/component/style.jsx
+++ src/component/style.jsx
@@ -1,12 +1,29 @@
 import styled from 'styled-components'
 
 export const Header = styled.header`
+position: relative;
 display: flex;
+flex-direction: column;
+gap: 20px;
 background-color: lightgreen;
-padding: 20px 40px;
+padding: 20px 40px 0px ;
 `
-export const div = styled.div`
-display: 'flex';
-justify-content: center;
+export const Div = styled.div`
+display: flex;
+justify-content: space-between;
 align-items: center;
+`
+export const DivColor = styled.div`
+background-color: brown;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 0 5px;
+color: white;
+`
+export const Input  = styled.input`
+padding: 10px;
+background-color: white;
+font-size: 1rem;
+color: #7c7c7c;
 `
