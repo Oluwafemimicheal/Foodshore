106ab6b270ceb3ea5eac2950b2fcde44f9099981
oluwafemimicheal <oluwafemicu@gmail.com>
2023-07-11 08:20:06 +0100

    initial commit

--- src/component/Homepage/Banner.jsx
+++ src/component/Homepage/Banner.jsx
@@ -4,7 +4,9 @@ import { Hero } from '../style';
 const Banner = () => {
     return (
       <Hero>
-
+        <div className="hero-container container">
+            <h1>We deliver your groceries <br/> to your front door</h1>
+        </div>
       </Hero>
     );
 }
