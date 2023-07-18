44ef81225d6737c4bebae904d93c16c0435d7939
oluwafemimicheal <oluwafemicu@gmail.com>
2023-07-18 16:05:51 +0100

    initial commit

--- src/component/style.jsx
+++ src/component/style.jsx
@@ -33,8 +33,8 @@ width: 100%;
 height: 80vh;
 `
 export const Item = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-wrap: wrap;
 `
