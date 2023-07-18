936c8d2716d84b30ea026dd3af4d4a6ae07e901e
oluwafemimicheal <oluwafemicu@gmail.com>
2023-07-18 22:58:05 +0100

    initial commit

--- src/component/Homepage/Items.jsx
+++ src/component/Homepage/Items.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
-import data from '../../../data'
 
-const Items = () => {
-    let item = [...data.products]
-    
-    let productItems = item.map((item)=>{
-
-        const {productImage, productName, price} = item
-        return`
-        <div className="card">
-        <div className="card-image">
-            <img src=${productImage} />
-        </div>
-        <div className="card-text">
-            <h1>${productName}</h1>
-        </div>
-        <div className="tag">
-            <small>${price}</small>
-        </div>
-    </div>
-        `
-    })
-
-    let items = productItems.join('')
-
+const Items = (img, text, tag) => {
     return (
        <>
-        {items}
+        <div className="card">
+            <div className="card-image">
+                <img src={img} />
+            </div>
+            <div className="card-text">
+                <h1>{text}</h1>
+            </div>
+            <div className="tag">
+                <small>{tag}</small>
+            </div>
+        </div>
         
        </>
     );
--- src/component/Homepage/Product.jsx
+++ src/component/Homepage/Product.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Item } from '../style';
 import Items from './Items';
+import data from '../../../data'
 
 const Product = () => {
+
+    const card = data.products.map(item =>{
+            return(
+                <Items
+                    key={item.id}
+                    img={item.productImage}
+                    title={item.productName}
+                    tag={item.price}
+                />
+            )
+    })
+
     return (
       <Item>
-        <Items/>
-        <Items/>
-        <Items/>
-        <Items/>
-        <Items/>
+        {card}
       </Item>
     
     );
