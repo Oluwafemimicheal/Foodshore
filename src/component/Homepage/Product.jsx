71cb891c7538b4bed321a35b8cbc0c3524c03950
oluwafemimicheal <oluwafemicu@gmail.com>
2023-07-19 12:34:48 +0100

    initial commit

--- src/component/Homepage/Product.jsx
+++ src/component/Homepage/Product.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Card from './card'
+import Card from './card';
+import data from '../../../data'
 
 
 const Product = () => {
     return (
         <div>
-            <Card/>
+            {data.products.map((item)=>{
+                const {productName, productImage, percentage, id}=item
+                <Card img={productImage} title={productName} off={percentage} key={id}/>
+            })
+            }
         </div>
     );
 }
--- src/component/Homepage/card.jsx
+++ src/component/Homepage/card.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import data from '../../../data'
 
-const Card = () => {
+const Card = (img, title, off) => {
     return (
         <div>
             <div className="card">
-                {data.products.map((item)=>{
-                    return(
-                        <div>
-                            <div className="image">
-                                <img src={item.productImage} alt="" />
-                            </div>
-                            <div className="text">
-                                <h1>{item.productName}</h1>
-                                <small>{item.percentage}</small>
-                                <a href="#" className='btn'>Order</a>
-                            </div>
-                        </div>
-                    )
-                })
-                }
-               
+                <div className="image">
+                    <img src={img} alt="" />
+                </div>
+                <div className="text">
+                    <h1>{title}</h1>
+                    <small>{off}</small>
+                    <a href="#" className='btn'>Order</a>
+                </div>
             </div>
         </div>
     );
