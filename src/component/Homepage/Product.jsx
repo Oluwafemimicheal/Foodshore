984e0d415d1d002a2be444975bd9a0c1077269a6
oluwafemimicheal <oluwafemicu@gmail.com>
2023-07-21 22:32:14 +0100

    initial commit

--- src/component/Homepage/Product.jsx
+++ src/component/Homepage/Product.jsx
@@ -29,37 +29,37 @@ const Product = () => {
             <Card img={productImage} title={productName} off={!percentage.length ? '' : percentage} price={price}  marketFrom={marketFrom} key={id}/>
         )
     });
-
     const frozenSection = data.familyPackage.map((item)=>{
         const {productImage, productName, percentage,price,  marketFrom, id} = item
         return(
             <Card img={productImage} title={productName} off={!percentage.length ? '' : percentage} price={price}  marketFrom={marketFrom} key={id}/>
         )
     })
+
     return (
         <Item>
             {!items.length ? "No Project" : items}
             <div className="package-section">
                 <h3>Family Packages</h3>
-                <div className="package-item flex">
+                <div className="package-item flex ">
                 {!familySection.length ? "No Family Package" : familySection}
                 </div>
             </div>
             <div className="package-section">
                 <h3>Single Man Packages</h3>
-                <div className="package-item flex">
+                <div className="package-item flex ">
                 {!singleSection.length ? "No Family Package" : singleSection}
                 </div>
             </div>
             <div className="package-section">
                 <h3>Light food Packages</h3>
-                <div className="package-item flex">
+                <div className="package-item flex ">
                 {!lightSection.length ? "No Family Package" : lightSection}
                 </div>
             </div>
             <div className="package-section">
                 <h3>Freezing Food </h3>
-                <div className="package-item flex">
+                <div className="package-item flex ">
                 {!frozenSection.length ? "No Family Package" : frozenSection}
                 </div>
             </div>
